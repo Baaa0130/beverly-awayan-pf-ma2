83a38196688588bcc5d252fbdfa28b4d099d0ab7
Baaa0130 <69729099+Baaa0130@users.noreply.github.com>
2020-09-27 23:10:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+//Question 1
+var petArray = ["cat","cow","dog"];
+
+//Question 2
+var name = "petArray"
+console.log (pet);
+
+//Question 3
+petArray.push("sheep");
+
+//Question 4
+var petArray = ["cat", "cow", "dog", "sheep"]
+console.log (petArray.length);
+
+//Question 5
+var catObject = {
+  name: "Raku", //name is the key, "Raku" is the value, a string value
+  colour: "Orange", // colour is the key, "Orange" is the value, a string value
+  age: 4, // age us the key, 4 is the value, a number value
+
+};
+
+//Question 6
+console.log (cat.age);
+// 4
+
+
+//Question 7
+var catObject = [{
+  name: "Raku", //name is the key, "Raku" is the value, a string value
+  colour: "Orange", // colour is the key, "Orange" is the value, a string value
+  age: 4, // age us the key, 4 is the value, a number value
+
+}
+];
+
+//Question 8
+for (let obj of catObject ){
+  console.log (catObject.name, catObject.colour, catObject.age)
+}
+
+//Question 9
+function logToConsole(x) {
+  console.log("value:"+x)
+
+}
+
+//Question 10
+logToConsole("donkey");
